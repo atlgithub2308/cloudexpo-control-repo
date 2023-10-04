a7a8bbd29f02dc6b0dab1b070499384064685cc3
Teck Leong Ang <teckleong.ang@-C02F50USMD6V.local>
2023-10-04 10:45:49 +0800

    added cem to default

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,9 +25,9 @@ File { backup => false }
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+
+  include cem_linux
+
 }
 
 
@@ -36,7 +36,5 @@ node pecloudexpo.atldemo.net {
 }
 
 node agent1cloudexpo.atldemo.net {
-
-  include cem_linux
 
 }
