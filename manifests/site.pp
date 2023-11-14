a61e4dea53ddc24de37c9cd3fc8034d5a10ace29
Teck Leong Ang <teckleong.ang@-C02F50USMD6V.local>
2023-11-14 15:42:03 +0800

    include cem

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,12 +30,12 @@ node default {
 
 node /^agent[\w._%+-]+/ {
 
-#  include cem_linux
+  include cem_linux
+
 
   user { 'fronixuser1':
     ensure  => 'present',
   }
-
 # package { 'tomcat':
 #    ensure => installed,
 #  }
